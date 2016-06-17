e701f939876be71c859385cf7d88bbafad0d2911
Andrew Charnley <ac@igaro.com>
2016-06-17 23:07:14 +0100

    Slight fix to route 404 test to diagnose codeship

--- tests/src/core.router.404.js
+++ tests/src/core.router.404.js
@@ -7,7 +7,7 @@ this['core.router.404'] = function(browser) {
   browser
     .url('http://localhost:3006/#/noexist')
     .waitForElementVisible('body', 1000)
-    .pause(500)
+    .pause(1500)
     .assert.elementPresent('.igaro-instance-modaldialog')
     .end();
 
